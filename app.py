776517399faa78e966273f1cc789a34295613b9b
raghavranjan001-dot <raghav.ranjan001@gmail.com>
2026-06-10 10:31:28 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -480,7 +480,8 @@ fetchCards();
 def index():
     return render_template_string(HTML)
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
